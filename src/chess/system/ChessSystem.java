abbf6b4747334ed437dae69872888d9c6d3fb024
Uemerson Negreiro da Silva <Negreiro273@gmail.com>
2024-12-10 11:02:29 -0400

    Starting to implement Board and Piece

--- src/chess/system/ChessSystem.java
+++ src/chess/system/ChessSystem.java
@@ -3,9 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package chess.system;
-
-import boardgame.Position;
-
 /**
  *
  * @author 90913370100
@@ -17,11 +14,6 @@ public class ChessSystem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
-        
-        
     }
     
 }
