49111d07c49eab511aabb178ed286a299cd5ffe9
Uemerson Negreiro da Silva <Negreiro273@gmail.com>
2024-12-10 15:11:09 -0400

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author 90913370100
+ */
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    
+    public BoardException(String msg){
+       super(msg);
+    }
+    
+}
