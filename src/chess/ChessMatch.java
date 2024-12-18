c161a8f9386673fcbe8d7d9c45e0463f80c027de
Uemerson Negreiro da Silva <Negreiro273@gmail.com>
2024-12-18 09:37:50 -0400

    Handling captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,8 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,6 +21,9 @@ public class ChessMatch {
     private int turn;
     private Color currentPalyer;
     private Board board;
+    
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces   = new ArrayList<>();
     
     
     public ChessMatch(){
@@ -75,6 +80,12 @@ public class ChessMatch {
        Piece p             = board.removePiece(source);
        Piece capturedPiece = board.removePiece(target);
        board.placePiece(p, target);
+       
+       if(capturedPiece != null){
+         piecesOnTheBoard.remove(capturedPiece);
+         capturedPiece.add(capturedPiece);
+       }
+       
        return capturedPiece;        
         
     }
@@ -111,6 +122,7 @@ public class ChessMatch {
     private void placeNewPiece(char column, int row, ChessPiece piece){
         
       board.placePiece(piece, new ChessPosition(column, row).toPosition()  );
+      piecesOnTheBoard.add(piece);
         
     }
     
--- src/chess/system/ChessSystem.java
+++ src/chess/system/ChessSystem.java
@@ -8,7 +8,9 @@ import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -24,13 +26,15 @@ public class ChessSystem {
         // TODO code application logic here
         ChessMatch chessMatch = new ChessMatch();
         Scanner sc = new Scanner(System.in);
+        List<ChessPiece> captured = new ArrayList<>();
+        
         
         
                 
         while(true){       
             try {
                     UI.clearScreen();
-                    UI.printMatch(chessMatch);
+                    UI.printMatch(chessMatch,captured);
                     System.out.println();
                     System.out.print("Source: ");
                     ChessPosition source = UI.readChessPosition(sc);
@@ -44,6 +48,11 @@ public class ChessSystem {
                     ChessPosition target = UI.readChessPosition(sc);
 
                     ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                    
+                    if(capturedPiece != null){
+                      captured.add(capturedPiece);
+                    }
+                    
 
             } catch (ChessException e) {
                 
